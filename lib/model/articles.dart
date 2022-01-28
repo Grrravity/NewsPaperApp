49deb137c26eae06a05e649fb6f9e39bdd302fb0
p.julienne <p.julienne@xefi.fr>
2022-01-28 13:42:41 +0100

    create database + models

---
+++ lib/model/articles.dart
@@ -0,0 +1,25 @@
+import 'package:newspaperapp/model/sources.dart';
+import 'package:sqfentity_gen/sqfentity_gen.dart';
+
+const articles = SqfEntityTable(
+    tableName: 'articles',
+    modelName: 'Articles',
+    primaryKeyName: 'uuid',
+    primaryKeyType: PrimaryKeyType.text,
+    fields: [
+      SqfEntityField('author', DbType.text),
+      SqfEntityField('title', DbType.text),
+      SqfEntityField('description', DbType.text),
+      SqfEntityField('url', DbType.text),
+      SqfEntityField('image_url', DbType.text),
+      SqfEntityField('published_at', DbType.datetime),
+      SqfEntityField('content', DbType.text),
+      SqfEntityField('created_at', DbType.datetime),
+      SqfEntityField('updated_at', DbType.datetime),
+      SqfEntityField('deleted_at', DbType.datetime),
+      SqfEntityFieldRelationship(
+          parentTable: sources,
+          deleteRule: DeleteRule.CASCADE,
+          fieldName: 'source_uuid',
+          isNotNull: true),
+    ]);
