1f5daeab98b16bdeb46ecaf74d310171f69ba6f6
Grrravity <julienne.prof@gmail.com>
2022-01-30 01:07:44 +0100

    ✨created main article image display

--- lib/Views/Widget/image_with_title.dart
+++ lib/Views/Widget/image_with_title.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:newspaperapp/Core/constants/ui_constants.dart';
 
 class ImageWithTitle extends StatelessWidget {
   final String imageUrl;
-  final String title;
-  final DateTime date;
+  final String? title;
+  final DateTime? date;
   final double? borderRadius;
   final double width;
   final double height;
+  final BoxFit? boxFit;
 
   ImageWithTitle._({
     required this.imageUrl,
-    required this.title,
-    required this.date,
     required this.width,
     required this.height,
+    this.title,
+    this.date,
     this.borderRadius,
+    this.boxFit,
   });
 
   factory ImageWithTitle.rounded({
     required String imageUrl,
-    required String title,
-    required DateTime date,
     required double width,
     required double height,
+    String? title,
+    DateTime? date,
+    BoxFit? boxFit = BoxFit.fill,
     double? borderRadius = 25,
   }) {
     return ImageWithTitle._(
@@ -34,22 +38,25 @@ class ImageWithTitle extends StatelessWidget {
       width: width,
       height: height,
       borderRadius: borderRadius,
+      boxFit: boxFit,
     );
   }
 
   factory ImageWithTitle.sharp({
     required String imageUrl,
-    required String title,
-    required DateTime date,
     required double width,
     required double height,
+    String? title,
+    DateTime? date,
+    BoxFit? boxFit = BoxFit.fill,
   }) {
     return ImageWithTitle._(
       imageUrl: imageUrl,
-      title: title,
-      date: date,
       width: width,
       height: height,
+      title: title,
+      date: date,
+      boxFit: boxFit,
     );
   }
 
@@ -60,39 +67,61 @@ class ImageWithTitle extends StatelessWidget {
         SizedBox(
           width: width,
           height: height,
-          child: Image.network(
-            imageUrl,
-            fit: BoxFit.fill,
-            loadingBuilder: (BuildContext context, Widget child,
-                ImageChunkEvent? loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              }
-              return Center(
-                child: CircularProgressIndicator(
-                  value: loadingProgress.expectedTotalBytes != null
-                      ? loadingProgress.cumulativeBytesLoaded /
-                          loadingProgress.expectedTotalBytes!
-                      : null,
-                ),
-              );
-            },
-            errorBuilder: (BuildContext context, Object exception,
-                StackTrace? stackTrace) {
-              return Image.asset('assets/images/imagenotfound.png');
-            },
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(borderRadius ?? 0),
+            child: Image.network(
+              imageUrl,
+              fit: boxFit,
+              loadingBuilder: (BuildContext context, Widget child,
+                  ImageChunkEvent? loadingProgress) {
+                if (loadingProgress == null) {
+                  return child;
+                }
+                return Center(
+                  child: LinearProgressIndicator(
+                    value: loadingProgress.expectedTotalBytes != null
+                        ? loadingProgress.cumulativeBytesLoaded /
+                            loadingProgress.expectedTotalBytes!
+                        : null,
+                  ),
+                );
+              },
+              errorBuilder: (BuildContext context, Object exception,
+                  StackTrace? stackTrace) {
+                return Image.asset(
+                  'assets/images/imagenotfound.png',
+                  fit: boxFit,
+                );
+              },
+            ),
           ),
         ),
         Positioned(
             bottom: 20,
             left: 20,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(title),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text(DateFormat('dd/MM/yyyy HH:mm').format(date))
+                if (title != null)
+                  SizedBox(
+                    width: width - 40,
+                    child: Text(
+                      title!,
+                      style: UiConstants.h5BoldWhite,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                    ),
+                  ),
+                if (title != null || date != null)
+                  const SizedBox(
+                    height: 8,
+                  ),
+                if (date != null)
+                  Text(
+                    DateFormat('dd/MM/yyyy HH:mm').format(date!),
+                    style: UiConstants.regularTextBold12
+                        .copyWith(color: UiConstants.backgroundWhite),
+                  )
               ],
             )),
       ],
